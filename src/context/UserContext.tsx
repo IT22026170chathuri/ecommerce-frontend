afbd4746c97dd9639a916ca5b5bebb3f6f7c45a3
IT22026170chathuri <135439160+IT22026170chathuri@users.noreply.github.com>
2025-10-23 22:43:31 +0530

    feat: connect Cart page with CartContext

--- src/context/UserContext.tsx
+++ src/context/UserContext.tsx
@@ -43,7 +43,7 @@ export const UserProvider = ({ children }: { children: ReactNode }) => {
 export const useUser = () => {
   const context = useContext(UserContext);
   if (!context) {
-    throw new Error('useUser must be used within UserProvider.');
+    throw new Error('useUser must be used within UserProvider');
   }
   return context;
 };
